ffab70388c4b42c88bccad03faa5123d77de72c5
Ensar Makas <ensar.makas@gmail.com>
2023-12-25 23:44:42 +0300

    Added UserDetails implementation for security

--- src/main/java/com/emakas/userService/service/UserService.java
+++ src/main/java/com/emakas/userService/service/UserService.java
@@ -4,12 +4,17 @@ import com.emakas.userService.model.User;
 import com.emakas.userService.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
-public class UserService extends BaseService<User, UUID> {
+public class UserService extends BaseService<User, UUID>  implements UserDetailsService{
 	private UserRepository repository;
     @Autowired
     public UserService(UserRepository repository) {
@@ -21,4 +26,15 @@ public class UserService extends BaseService<User, UUID> {
     	return repository.getByUserName(userName);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.getByUserName(username);
+        List<String> roles = new ArrayList<>();
+        roles.add("USER");
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUname())
+                .password(user.getPassword())
+                .roles(roles.toArray(new String[0]))
+                .build();
+    }
 }
