3ecd7f6768eda6a7b76a33312ef096f813b2cf36
Ensar Makas <ensar.makas@gmail.com>
2024-01-07 21:47:31 +0300

    Lombok applied

--- src/main/java/com/emakas/userService/model/UserRegistrationDto.java
+++ src/main/java/com/emakas/userService/model/UserRegistrationDto.java
@@ -1,7 +1,11 @@
 package com.emakas.userService.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
 public class UserRegistrationDto extends UserDto{
     private String name;
@@ -9,21 +13,4 @@ public class UserRegistrationDto extends UserDto{
 
     public UserRegistrationDto(){}
 
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
 }
