1db72597c7b3574b6873e19a7dcdf76b724422f8
Ensar Makas <ensar.makas@gmail.com>
2024-01-07 21:53:46 +0300

    Created sign in and sign up methods

--- src/main/java/com/emakas/userService/controller/UserController.java
+++ src/main/java/com/emakas/userService/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.emakas.userService.controller;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.UUID;
 
-import com.emakas.userService.model.LoginModel;
-import com.emakas.userService.model.Response;
-import com.emakas.userService.model.UserRegistrationDto;
-import com.emakas.userService.shared.AuthHelper;
+import com.emakas.userService.model.*;
+import com.emakas.userService.service.UserTokenService;
+import com.emakas.userService.shared.TokenManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +16,11 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import com.emakas.userService.model.User;
 import com.emakas.userService.service.UserService;
 
 @RestController
@@ -25,26 +28,33 @@ import com.emakas.userService.service.UserService;
 public class UserController {
 
     private final UserService userService;
+    private final UserTokenService userTokenService;
     private final AuthenticationManager authenticationManager;
+    private final TokenManager tokenManager;
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserController(UserService service, AuthenticationManager authenticationManager) {
+    public UserController(UserService service, UserTokenService userTokenService, AuthenticationManager authenticationManager, TokenManager tokenManager, PasswordEncoder passwordEncoder) {
         this.userService = service;
+        this.userTokenService = userTokenService;
         this.authenticationManager = authenticationManager;
+        this.tokenManager = tokenManager;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
     @PostMapping("sign-up")
     @ResponseBody
     public ResponseEntity<User> createUser(@RequestBody UserRegistrationDto userDto){
-        String passwordSalt = AuthHelper.generateRandomPasswordSalt();
-        String hashedPassword = AuthHelper.getHashedPassword(userDto.getPassword(), passwordSalt);
-    	User user = new User(
-                userDto.getUname(),
+        if (this.userService.existsByEmailOrUserName(userDto.getEmail(), userDto.getUserName()))
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        String hashedPassword = passwordEncoder.encode(userDto.getPassword());
+        User user = new User(
+                userDto.getUserName(),
                 userDto.getEmail(),
                 hashedPassword,
                 userDto.getName(),
-                userDto.getSurname(),
-                passwordSalt
+                userDto.getSurname()
         );
         userService.save(user);
     	return new ResponseEntity<>(user, HttpStatus.CREATED);
@@ -52,24 +62,34 @@ public class UserController {
 
     @PostMapping("sign-in")
     @ResponseBody
-    public ResponseEntity<Response<User>> signIn(@RequestBody LoginModel loginModel){
+    public ResponseEntity<Response<TokenResponse>> signIn(@RequestBody LoginModel loginModel){
         try{
+            UserDetails userDetails = this.userService.loadUserByUsername(loginModel.getUname());
+
             Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    loginModel.getUname(),
+                    userDetails.getUsername(),
                     loginModel.getPassword()
             ));
             if (auth.isAuthenticated()){
                 User user = this.userService.getByUserName(loginModel.getUname());
-                return new ResponseEntity<>(new Response<>(user), HttpStatus.OK);
+                UserToken userToken = tokenManager.createUserToken(
+                        user, Instant.now().plus(1, ChronoUnit.HOURS).getEpochSecond()
+                );
+                TokenResponse tokenResponse = new TokenResponse(
+                        user.getUserName(),
+                        user.getName(),
+                        user.getSurname(),
+                        user.getEmail(),
+                        userToken.getSerializedToken()
+                );
+                return new ResponseEntity<>(new Response<>(tokenResponse,"Login Success"), HttpStatus.OK);
             }
-            else {
-                return new ResponseEntity<>(new Response<>(null,"Invalid credentials"), HttpStatus.NOT_FOUND);
-            }
-        }catch (DisabledException exception){
-            return new ResponseEntity<>(new Response<>(null,"User is suspended"), HttpStatus.FORBIDDEN);
-        }
-        catch (BadCredentialsException exception){
             return new ResponseEntity<>(new Response<>(null,"Invalid credentials"), HttpStatus.NOT_FOUND);
+        }catch (UsernameNotFoundException | BadCredentialsException exception){
+            return new ResponseEntity<>(new Response<>(null,"Invalid credentials"), HttpStatus.NOT_FOUND);
+        }
+        catch (DisabledException exception){
+            return new ResponseEntity<>(new Response<>(null,"User is suspended"), HttpStatus.FORBIDDEN);
         }
     }
 
