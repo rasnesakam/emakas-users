321c4e9a83da5963a6fcfd2f0ee21b5e7d2b3cee
Ensar Makas <ensar.makas@gmail.com>
2024-01-12 13:01:00 +0300

    Base Entity setted up as inheritance mapping

--- src/main/java/com/emakas/userService/model/BaseEntity.java
+++ src/main/java/com/emakas/userService/model/BaseEntity.java
@@ -1,14 +1,11 @@
 package com.emakas.userService.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
@@ -16,7 +13,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@Entity
+@MappedSuperclass
 public class BaseEntity {
 
     @Id
