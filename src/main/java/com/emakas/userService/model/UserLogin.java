49eee369062cf933d5d7a48eb04f36b751d78c96
Ensar Makas <ensar.makas@gmail.com>
2024-01-29 11:37:24 +0300

    New modal added for saving user login transactions

--- src/main/java/com/emakas/userService/model/UserLogin.java
+++ src/main/java/com/emakas/userService/model/UserLogin.java
@@ -1,4 +1,56 @@
 package com.emakas.userService.model;
 
-public class UserLogin {
+import com.emakas.userService.shared.enums.Scope;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+public class UserLogin extends BaseEntity{
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "logged_user_id")
+    private User loggedUser;
+
+    @ElementCollection(targetClass = String.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "authorized_audiences", joinColumns = @JoinColumn(name = "authorized_audience_id"))
+    @Column
+    private Set<String> authorizedAudiences;
+
+    @Column
+    private Set<Scope> authorizedScopes;
+
+    @Column
+    @GeneratedValue
+    private UUID authorizationGrant;
+
+    @Column
+    private long expirationDateInSeconds;
+
+    /**
+     * Prepersist function will be executed before object being persisted.
+     * Set expirationDate 10 minutes after current date
+     */
+    @PrePersist
+    public void prePersist(){
+        if (this.expirationDateInSeconds == 0)
+            this.expirationDateInSeconds = Instant.now().plus( 10, ChronoUnit.MINUTES).getEpochSecond();
+    }
+
+    public UserLogin() {
+    }
+
+    public UserLogin(User loggedUser, Set<String> authorizedAudiences, Set<Scope> authorizedScopes) {
+        this.loggedUser = loggedUser;
+        this.authorizedAudiences = authorizedAudiences;
+        this.authorizedScopes = authorizedScopes;
+    }
 }
