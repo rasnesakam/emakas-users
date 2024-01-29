3f01f480207383b12724005bbc034abc916b425a
Ensar Makas <ensar.makas@gmail.com>
2024-01-29 11:40:12 +0300

    Added scope converter from string

--- src/main/java/com/emakas/userService/WebMvcConfig.java
+++ src/main/java/com/emakas/userService/WebMvcConfig.java
@@ -1,9 +1,25 @@
 package com.emakas.userService;
 
+import com.emakas.userService.shared.StringToScopeArrayConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final StringToScopeArrayConverter stringToScopeArrayConverter;
+
+    @Autowired
+    public WebMvcConfig(StringToScopeArrayConverter stringToScopeArrayConverter) {
+        this.stringToScopeArrayConverter = stringToScopeArrayConverter;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(stringToScopeArrayConverter);
+    }
 }
