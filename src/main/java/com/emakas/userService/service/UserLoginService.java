8645c88e56f847635237120f48c1f446659855bf
Ensar Makas <ensar.makas@gmail.com>
2024-01-29 11:38:30 +0300

    Added service for holding transactions

--- src/main/java/com/emakas/userService/service/UserLoginService.java
+++ src/main/java/com/emakas/userService/service/UserLoginService.java
@@ -1,4 +1,37 @@
 package com.emakas.userService.service;
 
+import com.emakas.userService.model.UserLogin;
+import com.emakas.userService.repository.UserLoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class UserLoginService {
+    private final UserLoginRepository userLoginRepository;
+
+    @Autowired
+    public UserLoginService(UserLoginRepository userLoginRepository) {
+        this.userLoginRepository = userLoginRepository;
+    }
+
+    public Optional<UserLogin> saveUserLogin(UserLogin userLogin){
+        try{
+            return Optional.of(userLoginRepository.save(userLogin));
+        }catch (Exception e){
+            return Optional.empty();
+        }
+    }
+
+    public Optional<UserLogin> getUserLoginByGrant(String grant){
+        UserLogin userLogin = userLoginRepository.findUserLoginByAuthorizationGrant(UUID.fromString(grant));
+        Instant exprirationInstant = Instant.ofEpochSecond(userLogin.getExpirationDateInSeconds());
+        Instant currentInstant = Instant.now();
+        if (exprirationInstant.isAfter(currentInstant))
+            return Optional.empty();
+        return Optional.of(userLogin);
+    }
 }
