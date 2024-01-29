565cb0808a12e5d09638df34f70607b1818936af
Ensar Makas <ensar.makas@gmail.com>
2024-01-29 11:37:55 +0300

    Added new repository for user login transactions

--- src/main/java/com/emakas/userService/repository/UserLoginRepository.java
+++ src/main/java/com/emakas/userService/repository/UserLoginRepository.java
@@ -1,4 +1,11 @@
 package com.emakas.userService.repository;
 
-public interface UserLoginRepository {
+import com.emakas.userService.model.UserLogin;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserLoginRepository extends BaseRepository<UserLogin, UUID> {
+    UserLogin findUserLoginByAuthorizationGrant(UUID authorizationGrant);
 }
