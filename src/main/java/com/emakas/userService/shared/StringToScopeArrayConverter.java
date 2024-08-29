3230f7c3dcadc4a0bec0de4d089dbc79b4f3b283
Ensar Makas <ensar.makas@gmail.com>
2024-08-29 22:06:12 +0300

    Added empty string control

--- src/main/java/com/emakas/userService/shared/StringToScopeArrayConverter.java
+++ src/main/java/com/emakas/userService/shared/StringToScopeArrayConverter.java
@@ -8,6 +8,8 @@ public class StringToScopeArrayConverter implements Converter<String, Scope[]> {
 
     @Override
     public Scope[] convert(String source) {
+        if (source.isEmpty())
+            return new Scope[0];
         String[] scopeStrings = source.split(",");
         Scope[] scopes = new Scope[scopeStrings.length];
 
