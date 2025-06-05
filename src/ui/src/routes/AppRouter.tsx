adcaf39f544ed3c5ace8a1d61aeaa862da37b03c
Ensar Makas <ensar.makas@gmail.com>
2025-06-05 23:39:42 +0300

    Page namings changed

--- src/ui/src/routes/AppRouter.tsx
+++ src/ui/src/routes/AppRouter.tsx
@@ -1,10 +1,11 @@
 import {Route, Routes} from "react-router";
 import {AuthLayout} from "@layouts/AuthLayout.tsx";
-import {Login} from "@pages/Auth/Login";
+import {LoginPage} from "@pages/Auth/Login";
 import {AppLayout} from "@layouts/AppLayout.tsx";
-import {Home} from "@pages/Home";
+import {HomePage} from "@pages/Home";
 import {UnderConstruction} from "@pages/UnderConstruction";
 import {NotFound} from "@pages/NotFound";
+import {AccountPage} from "@pages/Account";
 
 export function AppRouter (){
 
@@ -13,14 +14,14 @@ export function AppRouter (){
             <Route path={"page"}>
                 <Route path={"auth"}>
                     <Route element={<AuthLayout/>}>
-                        <Route path={"login"} element={<Login/>}/>
-                        <Route path={"logout"} element={<Login/>}/>
-                        <Route path={"sign-up"} element={<Login/>}/>
+                        <Route path={"login"} element={<LoginPage/>}/>
+                        <Route path={"logout"} element={<LoginPage/>}/>
+                        <Route path={"sign-up"} element={<LoginPage/>}/>
                     </Route>
                 </Route>
-                <Route index element={<Home />}/>
+                <Route index element={<HomePage />}/>
                 <Route element={<AppLayout/>}>
-                    <Route path={"account"} element={<UnderConstruction />} />
+                    <Route path={"account"} element={<AccountPage />} />
                     <Route path={"teams"} element={<UnderConstruction />} />
                     <Route path={"resources"} element={<UnderConstruction />} />
                     <Route path={"permissions"} element={<UnderConstruction />} />
