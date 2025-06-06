b1cfa2292a265a9e47214a80279d5b7aba77f9f4
Ensar Makas <ensar.makas@gmail.com>
2025-06-06 21:09:20 +0300

    Added Lombok annotations

--- src/main/java/com/emakas/userService/dto/TeamReadDto.java
+++ src/main/java/com/emakas/userService/dto/TeamReadDto.java
@@ -1,13 +1,22 @@
 package com.emakas.userService.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Collection;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TeamReadDto {
     private String name;
     private String description;
     private String uri;
     private TeamReadDto parentTeam;
     private Collection<TeamReadDto> childTeams;
-    private Collection<UserReadDto> users;
+    private Collection<UserReadDto> members;
     private UserReadDto lead;
 }
