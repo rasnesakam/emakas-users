c417bc49b89f9aa344987ecc62aae5cbdd683aea
Ensar Makas <ensar.makas@gmail.com>
2025-06-20 21:53:05 +0300

    Added token verification endpoint to filter rule

--- src/main/java/com/emakas/userService/requestFilters/AuthFilter.java
+++ src/main/java/com/emakas/userService/requestFilters/AuthFilter.java
@@ -36,7 +36,7 @@ public class AuthFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String url = request.getRequestURI();
-        return !url.startsWith("/api") || url.startsWith("/api/auth") || url.startsWith("/api/oauth");
+        return !url.startsWith("/api") || url.startsWith("/api/auth") || (url.startsWith("/api/oauth") && !url.startsWith("/api/oauth/token/verify"));
     }
 
     @Override
