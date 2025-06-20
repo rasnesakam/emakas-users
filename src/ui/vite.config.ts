778456096dc5149268c3c9a074502a2b5154900b
Ensar Makas <ensar.makas@gmail.com>
2025-06-20 23:32:07 +0300

    aliases added

--- src/ui/vite.config.ts
+++ src/ui/vite.config.ts
@@ -12,12 +12,13 @@ export default defineConfig({
       alias: {
           "@assets": path.resolve(__dirname, "src/assets/"),
           "@components": path.resolve(__dirname, "src/components/"),
-          "@hooks": path.resolve(__dirname, "src/hooks/"),
+          "@contexts": path.resolve(__dirname, "src/contexts/"),
           "@layouts": path.resolve(__dirname, "src/layouts/"),
           "@pages": path.resolve(__dirname, "src/pages/"),
           "@routes": path.resolve(__dirname, "src/routes/"),
           "@services": path.resolve(__dirname, "src/services/"),
-          "@utils": path.resolve(__dirname, "src/utils/")
+          "@utils": path.resolve(__dirname, "src/utils/"),
+          "@models": path.resolve(__dirname, "src/models")
       }
     }
 })
