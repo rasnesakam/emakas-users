ce83a96c538b107d41add928cbf05d777b7ddcc5
Ensar Makas <ensar.makas@gmail.com>
2025-07-29 20:03:41 +0300

    Added preauthorize and new endpoint

--- src/main/java/com/emakas/userService/controller/ApplicationsController.java
+++ src/main/java/com/emakas/userService/controller/ApplicationsController.java
@@ -11,11 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -33,20 +35,31 @@ public class ApplicationsController {
 
     @Operation(summary = "Register new application", security = @SecurityRequirement(name = "bearerAuth"))
     @PostMapping("/register")
+    @PreAuthorize("hasPermission(#RSC_APPS, 'self:write')")
     public ResponseEntity<Response<ApplicationDto>> createApplication(@RequestBody ApplicationDto applicationDto) {
         Application app = applicationDtoMapper.toApplication(applicationDto);
         app = applicationService.save(app);
         return ResponseEntity.ok(Response.of(applicationDtoMapper.toApplicationDto(app)));
     }
 
+    @Operation(summary = "Delete existing application", security = @SecurityRequirement(name = "bearerAuth"))
+    @DeleteMapping("/delete")
+    @PreAuthorize("hasPermission(#RSC_APPS, 'self:delete')")
+    public ResponseEntity<ApplicationDto> deleteApplication(@RequestParam(name = "id") UUID appId) {
+        Optional<Application> application = applicationService.getById(appId);
+        return application.map(app -> ResponseEntity.ok(applicationDtoMapper.toApplicationDto(app)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @Operation(summary = "Get registered applications", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/")
+    @PreAuthorize("hasPermission(#RSC_APPS, 'self:read')")
     public ResponseEntity<Collection<ApplicationDto>> getApplications() {
         Collection<ApplicationDto> applicationDtos = applicationService.getAll().stream().map(applicationDtoMapper::toApplicationDto).collect(Collectors.toSet());
         return ResponseEntity.ok(applicationDtos);
     }
 
-    @Operation(summary = "Get First Party Application", security = @SecurityRequirement(name = "bearerAuth"))
+    @Operation(summary = "Get First Party Application", security = @SecurityRequirement(name = "X-CSRF-TOKEN"))
     @GetMapping("/self")
     public ResponseEntity<ApplicationDto> getSelfApplication() {
         Optional<Application> application = applicationService.getByUri(appDomainName);
