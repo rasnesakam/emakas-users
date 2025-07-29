9bf9dd69d118c104817d57955c25acacb2794656
Ensar Makas <ensar.makas@gmail.com>
2025-07-29 20:03:17 +0300

    Added members controller

--- src/main/java/com/emakas/userService/controller/MembersController.java
+++ src/main/java/com/emakas/userService/controller/MembersController.java
@@ -2,12 +2,16 @@ package com.emakas.userService.controller;
 
 import com.emakas.userService.auth.JwtAuthentication;
 import com.emakas.userService.dto.Response;
+import com.emakas.userService.dto.TeamReadDto;
 import com.emakas.userService.dto.UserWriteDto;
+import com.emakas.userService.mappers.TeamsDtoMapper;
 import com.emakas.userService.mappers.UserDtoMapper;
 import com.emakas.userService.model.Team;
 import com.emakas.userService.model.User;
 import com.emakas.userService.service.TeamService;
 import com.emakas.userService.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,19 +31,22 @@ public class MembersController {
     private final UserService userService;
     private final TeamService teamService;
     private final UserDtoMapper userDtoMapper;
+    private final TeamsDtoMapper teamsDtoMapper;
     private static final Logger logger = LoggerFactory.getLogger(MembersController.class);
 
 
     @Autowired
-    public MembersController(UserService userService, TeamService teamService, UserDtoMapper userDtoMapper) {
+    public MembersController(UserService userService, TeamService teamService, UserDtoMapper userDtoMapper, TeamsDtoMapper teamsDtoMapper) {
         this.userService = userService;
         this.teamService = teamService;
         this.userDtoMapper = userDtoMapper;
+        this.teamsDtoMapper = teamsDtoMapper;
     }
 
-    @PreAuthorize("hasPermission(#RSC_TEAM_MEMBERS, 'self:create')")
+    @Operation(summary = "Assign new member to team", security = @SecurityRequirement(name = "bearerAuth"))
+    @PreAuthorize("hasPermission(#RSC_TEAM_MEMBERS, 'self:write')")
     @PostMapping("/assign/{team_uri}")
-    public ResponseEntity<Response<Team>> assignMemberOwnTeam(@RequestBody UserWriteDto userWriteDto, @PathVariable("team_uri") String teamUri) {
+    public ResponseEntity<TeamReadDto> assignMemberOwnTeam(@RequestBody UserWriteDto userWriteDto, @PathVariable("team_uri") String teamUri) {
         Optional<Team> intendedTeam = teamService.getByUri(teamUri);
         return intendedTeam.map(team -> {
             Optional<User> foundedUser = userService.getByUserName(userWriteDto.getUserName());
@@ -47,8 +54,17 @@ public class MembersController {
                 String returnMessage = String.format("User %s has assigned to team %s.", user.getUserName(), team.getName());
                 logger.info(returnMessage);
                 Team newTeam = teamService.addMemberToTeam(team, user);
-                return ResponseEntity.ok(Response.of(newTeam, returnMessage));
+                return ResponseEntity.ok(teamsDtoMapper.teamToTeamReadDto(newTeam));
             }).orElseGet(() -> ResponseEntity.notFound().build());
         }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @Operation(summary = "Get Team Information", security = @SecurityRequirement(name = "bearerAuth"))
+    @PreAuthorize("hasPermission(#RSC_TEAM_MEMBERS, 'self:read')")
+    @GetMapping("{team_uri}")
+    public ResponseEntity<TeamReadDto> getTeam(@PathVariable("team_uri") String teamUri){
+        Optional<Team> requestedTeam = teamService.getByUri(teamUri);
+        return requestedTeam.map(team -> ResponseEntity.ok(teamsDtoMapper.teamToTeamReadDto(team)))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
