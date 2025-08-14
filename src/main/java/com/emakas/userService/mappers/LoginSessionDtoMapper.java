800fde32463bb53661d3a63cc3ef0bdcd13d0666
Ensar Makas <ensar.makas@gmail.com>
2025-08-14 21:05:30 +0300

    Added custom mapping

--- src/main/java/com/emakas/userService/mappers/LoginSessionDtoMapper.java
+++ src/main/java/com/emakas/userService/mappers/LoginSessionDtoMapper.java
@@ -2,18 +2,43 @@ package com.emakas.userService.mappers;
 
 import com.emakas.userService.dto.LoginSessionDto;
 import com.emakas.userService.model.LoginSession;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import com.emakas.userService.service.LoginSessionService;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
-public interface LoginSessionDtoMapper {
-    static LoginSessionDtoMapper getInstance() { return Mappers.getMapper(LoginSessionDtoMapper.class);}
+import java.util.Optional;
 
-    LoginSession toLoginSession(LoginSessionDto dto);
+@Mapper(componentModel = "spring")
+public abstract class LoginSessionDtoMapper {
+    public LoginSessionDtoMapper getInstance() { return Mappers.getMapper(LoginSessionDtoMapper.class);}
+
+    @Mapping(target = "id", source = "sessionId")
+    @Mapping(target = "requestedClient", ignore = true)
+    @Mapping(target = "intendedUser", ignore = true)
+    @Mapping(target = "createdTime", ignore = true)
+    @Mapping(target = "updatedTime", ignore = true)
+    @Mapping(target = "expireDate", ignore = true)
+    public abstract LoginSession toLoginSession(LoginSessionDto dto, @Context LoginSessionService loginSessionService);
+
+    @AfterMapping
+    public void afterMappingToLoginSession(LoginSessionDto dto, @MappingTarget LoginSession session, @Context LoginSessionService loginSessionService) {
+        Optional<LoginSession> loginSessionValue = loginSessionService.getById(dto.getSessionId());
+        if (loginSessionValue.isPresent()) {
+            LoginSession loginSession = loginSessionValue.get();
+            session.setCreatedTime(loginSession.getCreatedTime());
+            session.setUpdatedTime(loginSession.getUpdatedTime());
+            session.setRequestedScopes(loginSession.getRequestedScopes());
+            session.setRedirectUri(loginSession.getRedirectUri());
+            session.setRequestedClient(loginSession.getRequestedClient());
+            session.setIntendedUser(loginSession.getIntendedUser());
+            session.setExpireDate(loginSession.getExpireDate());
+            session.setId(loginSession.getId());
+        }
+    }
 
     @Mapping(target = "sessionId", source = "id")
     @Mapping(target = "clientId", source = "requestedClient.id")
     @Mapping(target = "audience", source = "requestedClient.uri")
-    LoginSessionDto toLoginSessionDto(LoginSession session);
+    @Mapping(target = "state", ignore = true)
+    public abstract LoginSessionDto toLoginSessionDto(LoginSession session);
 }
