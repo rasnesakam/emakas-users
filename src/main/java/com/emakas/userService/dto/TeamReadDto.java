ec8701eff4e8713db5e89d665c89ddce224592b3
Ensar Makas <ensar.makas@gmail.com>
2025-09-26 22:00:53 +0300

    Added team id for identification

--- src/main/java/com/emakas/userService/dto/TeamReadDto.java
+++ src/main/java/com/emakas/userService/dto/TeamReadDto.java
@@ -7,12 +7,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TeamReadDto {
+    private UUID id;
+
     private String name;
 
     private String description;
