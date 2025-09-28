72ea703850c595b6cabdfdfad336354a87284c01
Ensar Makas <ensar.makas@gmail.com>
2025-09-28 14:27:51 +0300

    Updated page table, so granted resource permissions can be listed here

--- src/ui/src/pages/Permissions/index.tsx
+++ src/ui/src/pages/Permissions/index.tsx
@@ -1,7 +1,26 @@
 import {ResourcePermissionSliderForm} from "@components/SliderForms/ResourcePermission";
-import {Table, TableBody, TableCaption, TableHead, TableHeader, TableRow} from "@components/shadcn/ui/table.tsx";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from "@components/shadcn/ui/table.tsx";
+import {useEffect, useState} from "react";
+import {ResourcePermission} from "@models/Permissions.ts";
+import {PermissionTargetType} from "@utils/enums/PermissionTargetType.ts";
+import {getGrantedPermissions} from "@services/permissions";
+import {useAuthContext} from "@contexts/AuthContext";
 
 export function PermissionsPage() {
+    const {auth} = useAuthContext();
+    const [permissions, setPermissions] = useState<ResourcePermission[]>([])
+
+    useEffect(() => {
+        getGrantedPermissions(auth!).then(setPermissions);
+    }, []);
 
     return <>
 
@@ -19,13 +38,25 @@ export function PermissionsPage() {
                 <TableHeader>
                     <TableRow>
                         <TableHead>Resource Name</TableHead>
-                        <TableHead>Permission Type</TableHead>
-                        <TableHead>Permission Scope</TableHead>
                         <TableHead>Subject</TableHead>
                         <TableHead>Subject Type</TableHead>
+                        <TableHead>Access Modifier</TableHead>
+                        <TableHead>Permission Scope</TableHead>
                     </TableRow>
                 </TableHeader>
-                <TableBody></TableBody>
+                <TableBody>
+                    {permissions.map((item, index) => (<TableRow key={`permission-${index}`}>
+                        <TableCell>{item.resource.name}</TableCell>
+                        <TableCell>{
+                            item.permissionTargetType === PermissionTargetType.USER ? item.user.full_name :
+                                item.permissionTargetType === PermissionTargetType.TEAM ? item.team.name :
+                                    item.permissionTargetType === PermissionTargetType.APP ? item.application.name : ""
+                        }</TableCell>
+                        <TableCell>{item.permissionTargetType}</TableCell>
+                        <TableCell>{item.accessModifier}</TableCell>
+                        <TableCell>{item.permissionScope}</TableCell>
+                    </TableRow>))}
+                </TableBody>
             </Table>
         </div>
 
